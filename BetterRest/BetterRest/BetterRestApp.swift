c8467645a88a4f7de75a633ba0a025b61da9a2b5
Mario Alvarado <mario.alvarado.isw@gmail.com>
2023-10-06 18:36:53 -0600

    ADD Project 4, part 2

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Mario Alvarado on 03/09/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
